1a360c5ffab131dcfac4c08f46aa103a185df81c
Noah Cowan <nsncowan@gmail.com>
2023-12-26 10:57:06 -0800

    set up global state

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -16,4 +16,12 @@ const initialState = {
 export const GlobalContext = createContext(initialState);
 
 // provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  return (<GlobalContext.Provider value={{ 
+    transactions: state.transactions
+  }}>
+    {children}
+  </GlobalContext.Provider>)
+}
