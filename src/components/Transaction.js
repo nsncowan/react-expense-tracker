81d2b0819db9105e2e1d8b2c13c37ea5eabfe72e
Noah Cowan <nsncowan@gmail.com>
2023-12-26 11:52:15 -0800

    set up render transactions

---
+++ src/components/Transaction.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  return (
+    <div>
+      <li className="minus">
+        {transaction.text} <span>{sign}${Math.abs(transaction.amount)} </span><button className="delete-btn">x</button>
+      </li>
+    </div>
+  )
+}
